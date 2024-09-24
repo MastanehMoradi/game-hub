ad1592b8ee04263b4bfbd60dd3d74a34afa7812f
Masoomeh Moradi <masoomeh.moradi@afry.com>
2024-09-24 14:10:50 +0200

    displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Hide, Image } from '@chakra-ui/react';
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, Hide, Image, Text } from "@chakra-ui/react";
+import { PlatformIconList } from "./PlatformIconList";
 
 interface Props {
-    game: Game;
+  game: Game;
 }
 
-
-export const GameCard = ({game} :Props) => {
+export const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
-        <Image src={game.background_image}></Image>
-        <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
-        </CardBody>
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image}></Image>
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList platforms={game.parent_platforms.map(p=> p.platform)}></PlatformIconList>
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
